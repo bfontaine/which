aace3454574f03c36b7fdc618235c249c43a0a61
Baptiste Fontaine <batifon@yahoo.fr>
2015-07-28 22:58:57 +0200

    doc added

--- which/which.go
+++ which/which.go
@@ -5,14 +5,20 @@ import (
 	"path/filepath"
 )
 
+// One returns the first executable path matching the given command. It returns
+// an empty string if there's no such path.
 func One(cmd string) string {
 	return OneWithPath(cmd, os.Getenv("PATH"))
 }
 
+// All returns all executable paths. matching the given command. It returns nil
+// if there's no such path.
 func All(cmd string) []string {
 	return AllWithPath(cmd, os.Getenv("PATH"))
 }
 
+// OneWithPath is like One but it takes the PATH to check as a second argument
+// instead of using the PATH environnment variable.
 func OneWithPath(cmd, pathenv string) string {
 	paths := which(cmd, pathenv, true)
 
@@ -22,6 +28,8 @@ func OneWithPath(cmd, pathenv string) string {
 	return ""
 }
 
+// AllWithPath is like All but it takes the PATH to check as a second argument
+// instead of using the PATH environnment variable.
 func AllWithPath(cmd, pathenv string) []string {
 	return which(cmd, pathenv, false)
 }
